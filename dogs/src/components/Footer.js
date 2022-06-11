d42ffa8ecf638a97db9cd8d7ce4ba642723647c3
Jessica Gondim <jessicagbsg@gmail.com>
2022-06-10 23:46:53 -0300

    feed modal and photo

--- dogs/src/components/Footer.js
+++ dogs/src/components/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 export const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return <div className={styles.footer}></div>;
 };
 export default Footer;
